00646edbc1667f2576be7b97fb605cedab589f9e
Min Thet Naing <minthetnaing90@gmail.com>
2026-07-28 05:20:59 +0630

    chore: remove test scripts

--- src/notify/test-telegram.ts
+++
@@ -1,5 +0,0 @@
-import "dotenv/config";
-import { sendTelegramAlert } from "./telegram.ts";
-
-await sendTelegramAlert("🚨 Price drop! Abominable Hoodie dropped to $24.00\nhttps://scrapingcourse.com/ecommerce/product/abominable-hoodie");
-console.log("Sent!");
